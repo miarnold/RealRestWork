272d519cf74ed9e1ae82c1138a6c81ed5c1c0d93
Mack Arnold <miarnold@madisoncollege.edu>
2017-10-23 10:49:09 -0500

    done with 3/4 activities.

--- restdemo/src/test/java/TestServiceClient.java
+++ restdemo/src/test/java/TestServiceClient.java
@@ -1,3 +1,6 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.geocoder.Results;
+import com.google.geocoder.ResultsItem;
 import org.junit.Test;
 import javax.ws.rs.client.*;
 import javax.ws.rs.core.MediaType;
@@ -13,6 +16,12 @@ public class TestServiceClient {
                 client.target("http://maps.googleapis.com/maps/api/geocode/json?" +
                         "address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&sensor=false");
         String response = target.request(MediaType.APPLICATION_JSON).get(String.class);
-        assertEquals("???", response);
+
+        ObjectMapper mapper = new ObjectMapper();
+        Results results = mapper.readValue(response, Results.class);
+        ResultsItem resultsItem = results.getResults().get(0);
+        assertEquals(37.4216548, resultsItem.getGeometry().getLocation().getLat(),0);
+
+        //assertEquals("???", response);
     }
 }
